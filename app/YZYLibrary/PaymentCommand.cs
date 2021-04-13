bffb141c41a74754742ec1a01684df4dbb63c9ae
DESKTOP-Q0QRHC1\phili <philiprec@hotmail.com>
2021-04-13 17:00:59 -0400

    Updated PaymentCommand.cs and PaymentViewModel.cs

---
+++ app/YZYLibrary/PaymentCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace YZYLibrary
+{
+    class PaymentCommand : ICommand
+    {
+        Action _TargetExecuteMethod;
+        Func<bool> _TargetCanExecuteMethod;
+
+        public PaymentCommand(Action executeMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+            _TargetCanExecuteMethod = null;
+        }
+
+        public PaymentCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+            _TargetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+
+            if (_TargetCanExecuteMethod != null)
+            {
+                return _TargetCanExecuteMethod();
+            }
+
+            if (_TargetExecuteMethod != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // Beware - should use weak references if command instance lifetime is longer than lifetime of UI obj
+
+        // Prism commands solve this in their implementation 
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_TargetExecuteMethod != null)
+            {
+                _TargetExecuteMethod();
+            }
+        }
+    }
+}
---
+++ app/YZYLibrary/PaymentViewModel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YZYLibrary
+{
+    class PaymentViewModel
+    {
+        private YZYDbContext ctx;
+        public ObservableCollection<Payment> Payments { get; set; }
+        public PaymentCommand DeleteCommand { get; set; }
+        public PaymentCommand UpdateCommand { get; set; }
+        public PaymentCommand AddCommand { get; set; }
+
+        public PaymentViewModel()
+        {
+            Log.setLogOnFile();
+            try
+            {
+                ctx = new YZYDbContext();
+            }
+            catch (SystemException ex)
+            {
+                Log.WriteLine(ex.Message);
+                Environment.Exit(1);
+            }
+            Payments = new ObservableCollection<Payment>();
+            LoadPayments();
+            DeleteCommand = new PaymentCommand(this.OnDelete, this.CanExecute);
+            UpdateCommand = new PaymentCommand(this.OnUpdate, this.CanExecute);
+            AddCommand = new PaymentCommand(this.OnAdd, this.CanExecute);
+        }
+
+        private void LoadPayments()
+        {
+            try
+            {
+
+                var PaymentList = ctx.Payments.ToList();
+                Payments.Clear();
+                foreach (var item in PaymentList)
+                {
+                    Payments.Add(item);
+                }
+            }
+            catch (SystemException ex)
+            {
+                Log.WriteLine(ex.Message);
+            }
+        }
+
+        private Payment _selectedPayment;
+        public Payment SelectedPayment
+        {
+            get
+            {
+                return _selectedPayment;
+            }
+            set
+            {
+                _selectedPayment = value;
+            }
+        }
+
+        public void OnDelete()
+        {
+            ctx.Payments.Remove(SelectedPayment);
+            ctx.SaveChanges();
+            LoadPayments();
+        }
+        public bool CanExecute()
+        {
+            if (SelectedPayment != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void OnUpdate()
+        {
+            try
+            {
+                var item = (from r in ctx.Payments where r.PaymentID == SelectedPayment.PaymentID select r).FirstOrDefault<Payment>();
+                if (item != null)
+                {
+                    item.UserID = SelectedPayment.UserID;
+                    item.PayAccount = SelectedPayment.PayAccount;
+                    item.Amount = SelectedPayment.Amount;
+                    item.PayDate = SelectedPayment.PayDate;
+                }
+                ctx.SaveChanges();
+                LoadPayments();
+            }
+            catch (Exception ex)
+                when ((ex is InvalidParameterException) || (ex is SystemException))
+            {
+                Log.WriteLine(ex.Message);
+            }
+        }
+        public void OnAdd()
+        {
+            try
+            {
+                //WORKROUND: wdit a selected item to Add
+                //TOFIX: add item cannot reuse controller bound with list view
+                ctx.Payments.Add(SelectedPayment);
+                ctx.SaveChanges();
+                LoadPayments();
+            }
+            catch (Exception ex)
+                when ((ex is InvalidParameterException) || (ex is SystemException))
+            {
+                Log.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
