4613919bf6635c6b4069b92ee8d77627d65cd181
imtoam <imtoam.ca@users.noreply.github.com>
2021-04-13 11:11:31 -0400

    reset student gui project

--- app/YZYStudentGUI/Properties/AssemblyInfo.cs
+++ app/YZYStudentGUI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DotnetProject_YZY")]
+[assembly: AssemblyTitle("YZYStudentGUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotnetProject_YZY")]
+[assembly: AssemblyProduct("YZYStudentGUI")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
