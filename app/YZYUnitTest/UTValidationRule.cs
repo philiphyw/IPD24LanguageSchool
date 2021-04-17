fbec4af600c3526435239b795263e73eb2b5be98
imtoam <imtoam.ca@users.noreply.github.com>
2021-04-16 20:08:02 -0400

    Unit Test case example

---
+++ app/YZYUnitTest/UTValidationRule.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using YZYAdminGUI;
+using YZYLibrary;
+
+namespace YZYUnitTest
+{
+    [TestClass]
+    public class AdminLoginDialogTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidParameterException))]
+        public void checkCourseID_Failed()
+        {
+            ValidationRules.checkCourseID(-1);
+        }
+    }
+}
